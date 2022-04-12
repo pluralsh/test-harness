1a3f8a19c449cdbc2074ad7d1aa5afba8ee16302
michaelguarino <mguarino46@gmail.com>
2022-04-11 22:24:04 -0400

    Add mutual exclusion to socket connection

--- pkg/plural/websocket.go
+++ pkg/plural/websocket.go
@@ -5,41 +5,49 @@ import (
 	phx "github.com/Douvi/gophoenix"
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 type Socket struct {
+	mu        sync.Mutex
 	Client    *phx.Client
 	Config    *Config
 	Connected bool
 }
 
-type connectionReceiver struct {
-	client *Socket
+func (s *Socket) NotifyDisconnect() {
+	s.Connected = false
 }
 
-func (rc *connectionReceiver) NotifyDisconnect() {
-	rc.client.Connected = false
-}
-
-func (rc *connectionReceiver) NotifyConnect() {
-	rc.client.Connected = true
+func (s *Socket) NotifyConnect() {
+	s.Connected = true
 }
 
 func WebSocket(config *Config) (socket Socket) {
 	socket.Config = config
-	receiver := &connectionReceiver{client: &socket}
-	socket.Client = phx.NewWebsocketClient(receiver)
+	socket.Client = phx.NewWebsocketClient(&socket)
 	return
 }
 
 func (socket *Socket) Connect() error {
+	socket.mu.Lock()
+	defer socket.mu.Unlock()
+	if socket.Connected {
+		return nil
+	}
+
 	conf := socket.Config
 	url, err := url.Parse(fmt.Sprintf("wss://%s/socket/websocket?token=%s", conf.PluralEndpoint(), conf.Token))
 	if err != nil {
 		return err
 	}
 
-	return socket.Client.Connect(*url, http.Header{})
+	if err := socket.Client.Connect(*url, http.Header{}); err != nil {
+		return err
+	}
+
+	socket.Connected = true
+	return nil
 }
 
 func (socket *Socket) Join(callback phx.ChannelReceiver, topic string) (*phx.Channel, error) {
